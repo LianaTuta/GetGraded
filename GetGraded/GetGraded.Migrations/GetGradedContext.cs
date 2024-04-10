a3a93d875d16474e2f4ebacb63a095144bed19ea
LianaTuta <lianamaria20002@gmail.com>
2024-04-11 01:06:38 +0300

    Configure models for BL, DAL, Migration, DI

---
+++ GetGraded/GetGraded.Migrations/GetGradedContext.cs
@@ -0,0 +1,15 @@
+﻿using GetGraded.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetGraded.Migrations
+{
+    public class GetGradedContext : DbContext
+    {
+        public GetGradedContext(DbContextOptions<GetGradedContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<TestSave> Service { get; set; }
+    }
+}
---
+++ GetGraded/GetGraded/Extensions/DalExtension.cs
@@ -0,0 +1,16 @@
+﻿using GetGraded.BL.Services.Implementation;
+using GetGraded.BL.Services.Interface;
+using GetGraded.DAL.Repository.Implementation;
+using GetGraded.DAL.Repository.Interface;
+
+namespace GetGraded.Extensions
+{
+    public static class DalExtension
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ITestRepository, TestRepository>();
+            return services;
+        }
+    }
+}
